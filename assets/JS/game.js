fae7359bb0acf73b504139b00fc0137b112a72f4
brotherson67 <mitchell.brotherson@gmail.com>
2021-10-02 21:14:50 -0600

    I don't know what happened here

--- assets/JS/game.js
+++ assets/JS/game.js
@@ -67,11 +67,23 @@ var fight = function(enemyName) {
       );
   
       // check player's health
-      if (playerHealth <= 0) {
-        window.alert(playerName + ' has died!');
-        // leave while() loop if player is dead
-        break;
-      } else {
+      if (playerHealth > 0) {
+        // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
+        window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+    
+        // pick new enemy to fight based on the index of the enemyNames array
+        var pickedEnemyName = enemyNames[i];
+    
+        // reset enemyHealth before starting new fight
+        enemyHealth = 50;
+    
+        // use debugger to pause script from running and check what's going on at that moment in the code
+        // debugger;
+    
+        // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
+        fight(pickedEnemyName);
+      }
+       else {
         window.alert(playerName + ' still has ' + playerHealth + ' health left.');
       }
     }
@@ -94,7 +106,10 @@ var fight = function(enemyName) {
 // }
 
 for (var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(pickedEnemyName);
+    // var pickedEnemyName = enemyNames[i];
+    // enemyHealth = 50;
+    // fight(pickedEnemyName);
+    if (playerHealth > 0) {
+        window.alert("Welcome to Robot Gladiators!" + (i + 1));
+      }
 }
